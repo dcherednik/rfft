553dfc055893c8b19ba434f376729aec8afbcce3
Daniil Cherednik <dan.cherednik@gmail.com>
2018-04-30 19:26:47 +0000

    Initial RFFT implementation

---
+++ src/lib.rs
@@ -0,0 +1,65 @@
+pub extern crate num_complex;
+extern crate rustfft;
+extern crate num;
+
+use num_complex::Complex;
+
+pub mod rfft;
+pub use rfft::{RFFTImpl, RIFFTImpl};
+pub use rustfft::{FFTnum, Length};
+
+pub trait RFFT<T: FFTnum>: Length + Sync + Send {
+    fn process(&self, input: &mut [T], output: &mut [Complex<T>]);
+}
+
+pub trait RIFFT<T: FFTnum>: Length + Sync + Send {
+    fn process(&self, input: &mut [Complex<T>], output: &mut [T]);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rustfft::FFT;
+    use self::num::complex::Complex;
+    use rustfft::num_traits::Zero;
+    use std::f64::consts::PI;
+    use rustfft::algorithm::Radix4;
+
+
+    fn check_rfft(mut src: Vec<f64>) {
+        let mut input2: Vec<Complex<f64>> = vec![Zero::zero(); src.len()];
+        for i in 0..src.len() {
+            input2[i].re = src[i];
+        }
+        let mut spectrum1: Vec<Complex<f64>> = vec![Zero::zero(); src.len() / 2];
+        let mut spectrum2: Vec<Complex<f64>> = vec![Zero::zero(); src.len()];
+
+        let ref_fft = Radix4::new(src.len(), false);
+        ref_fft.process(&mut input2, &mut spectrum2);
+
+        let rfft = RFFTImpl::new(src.len());
+        rfft.process(&mut src, &mut spectrum1);
+
+        for i in 0..src.len() / 2 {
+            assert_eq!((10000f64 * spectrum1[i].re).round(), (10000f64 * spectrum2[i].re).round());
+            assert_eq!((10000f64 * spectrum1[i].im).round(), (10000f64 * spectrum2[i].im).round());
+        }
+
+        let mut tmp: Vec<f64> = vec![Zero::zero(); src.len()];
+        let rifft = RIFFTImpl::new(src.len());
+        rifft.process(&mut spectrum1, &mut tmp);
+        for i in 0..src.len() {
+            assert_eq!((10000f64 * src[i]).round(), (10000f64 * tmp[i]/2048.0).round());
+        }
+    }
+    #[test]
+    fn rfft_test() {
+        let mut src: Vec<f64> = vec![Zero::zero(); 4096];
+        for i in 0..4096 {
+            src[i] = ((PI * (i as f64) / 512 as f64).cos()) as f64;
+        }
+        check_rfft(src);
+    }
+}
+
+
---
+++ src/rfft.rs
@@ -0,0 +1,142 @@
+extern crate num;
+
+use std::f64::consts::PI;
+use rustfft::{FFT, FFTnum};
+use rustfft::algorithm::Radix4;
+use rfft::num::complex::Complex;
+use rustfft::num_traits::{Zero, FromPrimitive};
+
+use ::{RFFT, RIFFT, Length};
+
+pub struct RFFTImpl<T> {
+    len: usize,
+    cos: Box<[T]>,
+    fft: Box<FFT<T>>
+}
+
+pub struct RIFFTImpl<T> {
+    len: usize,
+    cos: Box<[T]>,
+    fft: Box<FFT<T>>
+}
+
+fn gen_cos<T: FFTnum>(len: usize) -> Vec<T> {
+    let mut cos_table = Vec::with_capacity(len);
+    let n = len * 2;
+    for i in 0..len {
+        cos_table.push(FromPrimitive::from_f64((PI * (i as f64) / n as f64).cos()).unwrap());
+    }
+    return cos_table;
+}
+
+impl<T: FFTnum> RFFTImpl<T> {
+    pub fn new(len: usize) -> Self {
+        let cos = gen_cos(len / 4);
+        RFFTImpl {
+            len: len,
+            cos: cos.into_boxed_slice(),
+            fft: Box::new(Radix4::new(len / 2, false)),
+        }
+    }
+}
+
+impl<T: FFTnum> RFFT<T> for RFFTImpl<T> {
+
+    fn process(&self, input: &mut [T], mut output: &mut [Complex<T>]) {
+        let mut tmp: Vec<Complex<T>> = vec![Zero::zero(); self.len / 2];
+        for i in 0..self.len / 2 {
+            tmp[i].re = input[2 * i];
+            tmp[i].im = input[2 * i + 1];
+        }
+
+        self.fft.process(&mut tmp, &mut output);
+
+        let dc = output[0].re;
+        output[0].re = dc + output[0].im;
+        output[0].im = dc - output[0].im;
+
+        let end_table = self.len / 4;
+        let m: T = FromPrimitive::from_f64(0.5f64).unwrap();
+
+        for i in 1..end_table {
+            let cos_idx = i;
+            let sin_idx = end_table - i;
+            let j = self.len / 2 - i;
+
+            let a_re = m * (output[i].re + output[j].re);
+            let b_im = m * (output[j].re - output[i].re);
+            let a_im = m * (output[i].im - output[j].im);
+            let b_re = m * (output[i].im + output[j].im);
+            let sum_re = b_re * self.cos[cos_idx] + b_im * self.cos[sin_idx];
+            let sum_im = b_im * self.cos[cos_idx] - b_re * self.cos[sin_idx];
+            output[i].re = a_re + sum_re;
+            output[i].im = a_im + sum_im;
+            output[j].re = a_re - sum_re;
+            output[j].im = sum_im - a_im;
+        }
+    }
+}
+
+impl<T> Length for RFFTImpl<T> {
+    #[inline(always)]
+    fn len(&self) -> usize {
+        self.len
+    }
+}
+
+impl<T: FFTnum> RIFFTImpl<T> {
+    pub fn new(len: usize) -> Self {
+        let cos = gen_cos(len / 4);
+        RIFFTImpl {
+            len: len,
+            cos: cos.into_boxed_slice(),
+            fft: Box::new(Radix4::new(len / 2, true)),
+        }
+    }
+}
+
+impl<T: FFTnum> RIFFT<T> for RIFFTImpl<T> {
+    fn process(&self, mut input: &mut [Complex<T>], output: &mut [T]) {
+
+        let dc = input[0].re;
+        let m: T = FromPrimitive::from_f64(0.5f64).unwrap();
+        input[0].re = m * (dc + input[0].im);
+        input[0].im = m * (dc - input[0].im);
+
+        let end_table = self.len / 4;
+
+        for i in 1..end_table {
+            let cos_idx = i;
+            let sin_idx = end_table - i;
+            let j = self.len / 2 - i;
+
+            let a_re = m * (input[i].re + input[j].re);
+            let b_im = m * (input[i].re - input[j].re);
+            let a_im = m * (input[i].im - input[j].im);
+            let b_re = m * (-input[i].im - input[j].im);
+
+            let sum_re = b_re * self.cos[cos_idx] - b_im * self.cos[sin_idx];
+            let sum_im = b_im * self.cos[cos_idx] + b_re * self.cos[sin_idx];
+            input[i].re = a_re + sum_re;
+            input[i].im = a_im + sum_im;
+            input[j].re = a_re - sum_re;
+            input[j].im = sum_im - a_im;
+        }
+
+        let mut tmp: Vec<Complex<T>> = vec![Zero::zero(); self.len / 2];
+        self.fft.process(&mut input, &mut tmp);
+
+        for i in 0..self.len / 2 {
+            output[2 * i] = tmp[i].re;
+            output[2 * i + 1] = tmp[i].im;
+        }
+    }
+}
+
+impl<T> Length for RIFFTImpl<T> {
+    #[inline(always)]
+    fn len(&self) -> usize {
+        self.len
+    }
+}
+
